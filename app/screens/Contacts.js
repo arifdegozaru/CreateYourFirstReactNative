05bb2a4d3d93aae9f224536f5e73b509982efe86
Arif Hidayatullah <arif.hidayatullah@mitrais.com>
2018-12-30 00:11:38 +0700

    Contact Screen

--- app/screens/Contacts.js
+++ app/screens/Contacts.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
-import {
-  View,
-  Text
-} from 'react-native'
+import { FlatList } from 'react-native'
+
+import { contacts } from '../config/data'
+import colors from '../config/colors'
+import { ListItem } from '../components/ListItem'
 
 class Contacts extends Component {
-  render() {
+
+  handleRowPress = (item) => {
+    this.props.navigation.navigate('Details', item)
+  }
+
+  render () {
     return (
-      <View>
-        <Text>Contacts Screen</Text>
-      </View>
-    );
+      <FlatList
+        style={{ backgroundColor: colors.background }}
+        data={contacts}
+        renderItem={({ item }) =>
+          <ListItem contact={item} onPress={() => {this.handleRowPress(item)}}/>
+        }
+        keyExtractor={(item) => item.email}
+      />
+    )
   }
 }
 
