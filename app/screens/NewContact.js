f11cd4bb5a72a90401b4fe8eb8f56edc7aea857c
Arif Hidayatullah <arif.hidayatullah@mitrais.com>
2018-12-30 14:49:04 +0700

    New Contact Screen

--- app/screens/NewContact.js
+++ app/screens/NewContact.js
@@ -1,16 +1,60 @@
 import React, { Component } from 'react'
-import {
-  View,
-  Text
-} from 'react-native'
+import { ScrollView, View } from 'react-native'
+
+import colors from '../config/colors'
+import { TextInput } from '../components/TextInput'
+import { PrimaryButton } from '../components/Buttons'
+
+const fields = [
+  { placeholder: 'First Name', stateKey: 'firstName' },
+  { placeholder: 'Last Name', stateKey: 'lastName' },
+  { placeholder: 'Email', stateKey: 'email', keyboardType: 'email-address' },
+  { placeholder: 'Mobile Phone', stateKey: 'mobilePhone' },
+  { placeholder: 'Home Phone', stateKey: 'homePhone' },
+  { placeholder: 'City', stateKey: 'city' },
+  { placeholder: 'Birthday', stateKey: 'birthday' },
+  { placeholder: 'Registered', stateKey: 'registered' },
+  { placeholder: 'Username', stateKey: 'username' },
+]
 
 class NewContact extends Component {
-  render() {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  onInputChange = (text, stateKey) => {
+    const mod = {}
+    mod[stateKey] = text
+    this.setState(mod)
+  }
+
+  handleSubmit = () => {
+    alert('Submit')
+  }
+
+  render () {
     return (
-      <View>
-        <Text>NewContact Screen</Text>
-      </View>
-    );
+      <ScrollView style={{ backgroundColor: colors.background }}>
+        {
+          fields.map((field) => (
+            <TextInput
+              key={field.stateKey}
+              onChange={(text) => this.onInputChange(text, field.stateKey)}
+              {...field}
+            />
+          ))
+        }
+        <View style={{ marginTop: 20 }}>
+          <PrimaryButton
+            text='Save'
+            onPress={() => this.handleSubmit()}
+          />
+        </View>
+      </ScrollView>
+    )
   }
 }
 
